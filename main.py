266312d0ae1b45646d938ddb76b12905c98b082b
tytjan <adrianczech-1997@wp.pl>
2022-01-11 23:06:15 +0100

    Zautomatyzowane instalowanie pygame.

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
+import sys
+import subprocess
+import pygame
+
 from Game import Game
 
-game = Game(screen_width=800, screen_height=600)
-game.play()
+
+def install_pygame():
+    print("Installing pygame. Please wait...")
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "pygame"])
+
+
+def main():
+    install_pygame()
+    game = Game(screen_width=800, screen_height=600)
+    game.play()
+
+
+if __name__ == "__main__":
+    main()
